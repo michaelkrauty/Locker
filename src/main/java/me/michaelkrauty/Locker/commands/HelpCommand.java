5052f50f4e64ceb4e6a05312548ec33e5ecf4d74
agog27 <brixwhiz@gmail.com>
2014-07-12 16:24:18 -0400

    first agog27 commit

--- src/main/java/me/michaelkrauty/Locker/commands/HelpCommand.java
+++ src/main/java/me/michaelkrauty/Locker/commands/HelpCommand.java
@@ -13,12 +13,13 @@ import org.bukkit.entity.Player;
 public class HelpCommand {
 
 	public HelpCommand(Main instance, Player player, Command cmd, String commandLabel, String[] args) {
-		player.sendMessage(ChatColor.GRAY + "---help & stuff---");
-		player.sendMessage(ChatColor.GRAY + "**Use commands looking directly at any chest**");
-		player.sendMessage(ChatColor.RED + "/locker lock " + ChatColor.GRAY + "lock a chest");
-		player.sendMessage(ChatColor.RED + "/locker unlock " + ChatColor.GRAY + "unlock a chest");
-		player.sendMessage(ChatColor.RED + "/locker users " + ChatColor.GRAY + "see who can access a chest");
-		player.sendMessage(ChatColor.RED + "/locker users " + ChatColor.RED + "<add/remove> " + ChatColor.GRAY + "manage chest access");
-		player.sendMessage(ChatColor.GREEN + "-" + ChatColor.RED + "Breaking a locked chest will also break the lock.");
+		player.sendMessage(ChatColor.DARK_GRAY + "------" + ChatColor.GRAY + "[ " + ChatColor.DARK_PURPLE + "Locker Help "+ ChatColor.GRAY + "]" + ChatColor.DARK_GRAY + "------");
+		player.sendMessage(ChatColor.RED + "** " + ChatColor.LIGHT_PURPLE + "Use commands looking directly at any chest " + ChatColor.LIGHT_PURPLE + "**");
+		player.sendMessage(ChatColor.LIGHT_PURPLE + "/locker lock " + ChatColor.GRAY + "lock the targeted chest");
+		player.sendMessage(ChatColor.LIGHT_PURPLE + "/locker unlock " + ChatColor.GRAY + "unlock the targeted chest" + ChatColor.DARK_GRAY + " (" + ChatColor.GRAY+ "you must be its owner" + ChatColor.DARK_GRAY + ")");
+		player.sendMessage(ChatColor.LIGHT_PURPLE + "/locker users " + ChatColor.GRAY + "see who can access the targeted chest");
+		player.sendMessage(ChatColor.LIGHT_PURPLE + "/locker users " + ChatColor.DARK_PURPLE + "<add/remove> " + ChatColor.GRAY + "manage chest access");
+		player.sendMessage(ChatColor.GRAY + "- " + ChatColor.RED + "Your chest protection will expire " + ChatColor.DARK_RED + "30 Days " + ChatColor.RED + "after being locked.");
+		player.sendMessage(ChatColor.GRAY + "- " + ChatColor.RED + "Destroying a locked chest with explosives will break the lock.");
 	}
 }
